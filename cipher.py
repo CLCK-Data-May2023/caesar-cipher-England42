48220f4e280d3c13bacef401f8f9ddd14105cb33
England42 <134561463+England42@users.noreply.github.com>
2023-08-06 19:24:00 -0400

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,1 +1,15 @@
-# add your code here
+def caesar_cipher(text, shift=5):
+    result = ""
+
+    for char in text:
+        if char.isalpha():
+            ascii_offset = ord('a') if char.islower() else ord('A')
+            result += chr((ord(char) - ascii_offset + shift) % 26 + ascii_offset)
+        else:
+            result += char
+
+    return result
+
+plain_text = input("Enter a sentence to encrypt: ")
+encrypted_text = caesar_cipher(plain_text)
+print("The encrypted text is:", encrypted_text)
